283fab37f114eaade0a649edd66fa169ba425284
Elliot Thomas <elliot@raziel.dev>
2020-10-12 18:04:59 +0100

    Removed seeding from main

--- main.go
+++ main.go
@@ -4,16 +4,10 @@ import (
 	"github.com/raziel2244/geckosite/database"
 	"github.com/raziel2244/geckosite/router"
 	"github.com/raziel2244/geckosite/s3"
-	"github.com/raziel2244/geckosite/seed"
 )
 
 func main() {
-	seed.Rand()
-
 	database.Init()
-	// seed.Database()
-
 	s3.Init()
-
 	router.InitAndServe()
 }
