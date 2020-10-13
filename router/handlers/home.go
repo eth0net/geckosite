4b6128586b88c5f2c09b6cf668809e8b463927d9
Elliot Thomas <elliot@raziel.dev>
2020-10-13 18:17:53 +0100

    Added links and welcome to home

--- router/handlers/home.go
+++ router/handlers/home.go
@@ -7,7 +7,20 @@ import (
 
 // Home returns the home page.
 func Home(w http.ResponseWriter, r *http.Request) {
+	type card struct{ Title, Image, Link string }
+	type page struct {
+		Cards []card
+	}
+
+	data := page{
+		Cards: []card{
+			{"Crested Geckos", "/static/img/hhicon.png", "/geckos/crested"},
+			{"Gargoyle Geckos", "/static/img/hhicon.png", "/geckos/gargoyle"},
+			{"Leopard Geckos", "/static/img/hhicon.png", "/geckos/leopard"},
+		},
+	}
+
 	lp, hp := "templates/layout.gohtml", "templates/home.gohtml"
 	tmpl := template.Must(template.ParseFiles(lp, hp))
-	tmpl.ExecuteTemplate(w, "layout", nil)
+	tmpl.ExecuteTemplate(w, "layout", data)
 }
