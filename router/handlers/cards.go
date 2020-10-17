b15356ac98d24702981c02e5d51e05f654ee1558
Elliot Thomas <elliot@raziel.dev>
2020-10-17 17:54:32 +0100

    Images now work for cards pages

--- router/handlers/cards.go
+++ router/handlers/cards.go
@@ -1,15 +1,23 @@
 package handlers
 
 import (
+	"context"
 	"html/template"
+	"math/rand"
 	"net/http"
+	"strings"
+	"time"
 
 	"github.com/gorilla/mux"
+	"github.com/minio/minio-go/v7"
+	"github.com/raziel2244/geckosite/database"
+	"github.com/raziel2244/geckosite/database/model"
+	"github.com/raziel2244/geckosite/s3"
 )
 
 // Cards returns a page containing cards for child routes.
 func Cards(w http.ResponseWriter, r *http.Request) {
-	type card struct{ Title, Image, Link string }
+	type card struct{ Title, Path, Image string }
 	type page struct {
 		Title string
 		Cards []card
@@ -19,24 +27,24 @@ func Cards(w http.ResponseWriter, r *http.Request) {
 	pages := map[string]pageMap{
 		"geckos": {
 			"": {"Geckos", []card{
-				{"Crested Geckos", "/static/img/hhicon.png", "/geckos/crested"},
-				{"Gargoyle Geckos", "/static/img/hhicon.png", "/geckos/gargoyle"},
-				{"Leopard Geckos", "/static/img/hhicon.png", "/geckos/leopard"},
+				{"Crested Geckos", "/geckos/crested", ""},
+				{"Gargoyle Geckos", "/geckos/gargoyle", ""},
+				{"Leopard Geckos", "/geckos/leopard", ""},
 			}},
 			"crested": {"Crested Geckos", []card{
-				{"Personal", "/static/img/hhicon.png", "/geckos/crested/personal"},
-				{"Holdbacks", "/static/img/hhicon.png", "/geckos/crested/holdbacks"},
-				{"For Sale", "/static/img/hhicon.png", "/geckos/crested/for-sale"},
+				{"Personal", "/geckos/crested/personal", ""},
+				{"Holdbacks", "/geckos/crested/holdbacks", ""},
+				{"For Sale", "/geckos/crested/for-sale", ""},
 			}},
 			"gargoyle": {"Gargoyle Geckos", []card{
-				{"Personal", "/static/img/hhicon.png", "/geckos/gargoyle/personal"},
-				{"Holdbacks", "/static/img/hhicon.png", "/geckos/gargoyle/holdbacks"},
-				{"For Sale", "/static/img/hhicon.png", "/geckos/gargoyle/for-sale"},
+				{"Personal", "/geckos/gargoyle/personal", ""},
+				{"Holdbacks", "/geckos/gargoyle/holdbacks", ""},
+				{"For Sale", "/geckos/gargoyle/for-sale", ""},
 			}},
 			"leopard": {"Leopard Geckos", []card{
-				{"Personal", "/static/img/hhicon.png", "/geckos/leopard/personal"},
-				{"Holdbacks", "/static/img/hhicon.png", "/geckos/leopard/holdbacks"},
-				{"For Sale", "/static/img/hhicon.png", "/geckos/leopard/for-sale"},
+				{"Personal", "/geckos/leopard/personal", ""},
+				{"Holdbacks", "/geckos/leopard/holdbacks", ""},
+				{"For Sale", "/geckos/leopard/for-sale", ""},
 			}},
 		},
 	}
@@ -54,6 +62,75 @@ func Cards(w http.ResponseWriter, r *http.Request) {
 	if _, ok := pages[o][t]; !ok {
 		NotFound(w, r)
 		return
+	}
+
+	rand.Seed(time.Now().Unix())
+
+	pageData := pages[o][t]
+	for c, card := range pageData.Cards {
+		splitPath := strings.Split(card.Path, "/")
+
+		var species model.Species
+		database.DB.First(&species, &model.Species{Order: o, Type: splitPath[2]})
+
+		var animals []*model.Animal
+		if len(splitPath) == 3 {
+			// check for sale
+			// check holdbacks
+			// check personal
+
+			wheres := []string{
+				"status = 'For Sale'",
+				"status = 'Holdback'",
+				"status IN ('Non-Breeder','Breeder','Future Breeder')",
+			}
+
+			for _, where := range wheres {
+				database.DB.Where("species_id = ? AND "+where, species.ID.String()).Find(&animals)
+				if len(animals) > 0 {
+					break
+				}
+			}
+		} else {
+			// check category
+
+			wheres := map[string]string{
+				"for-sale":  "status = 'For Sale'",
+				"holdbacks": "status = 'Holdback'",
+				"personal":  "status IN ('Non-Breeder','Breeder','Future Breeder')",
+			}
+
+			where := wheres[splitPath[3]]
+			database.DB.Find(&animals, "species_id = ? AND "+where, species.ID)
+		}
+
+		if len(animals) == 0 {
+			pageData.Cards[c].Image = "/static/img/coming-soon.jpg"
+			continue
+		}
+
+		for _, animal := range animals {
+			// get images for animal
+			var images []string
+			ch := s3.Client.ListObjects(
+				context.Background(),
+				o,
+				minio.ListObjectsOptions{
+					Prefix:    splitPath[2] + "/" + animal.ID.String(),
+					Recursive: true,
+				},
+			)
+			for object := range ch {
+				path := "/s3/" + o + "/" + object.Key
+				images = append(images, path)
+			}
+
+			// pick random one for card
+			if len(images) > 0 {
+				pageData.Cards[c].Image = images[rand.Intn(len(images))]
+				break
+			}
+		}
 	}
 
 	lp, hp := "templates/layout.gohtml", "templates/cards.gohtml"
