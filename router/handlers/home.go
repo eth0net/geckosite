4deeaaad4c1769e30d52ecdcdde1cad11c59fca3
Elliot Thomas <elliot@raziel.dev>
2020-10-17 17:59:43 +0100

    Images now work for home page

--- router/handlers/home.go
+++ router/handlers/home.go
@@ -1,23 +1,89 @@
 package handlers
 
 import (
+	"context"
 	"html/template"
+	"math/rand"
 	"net/http"
+	"strings"
+	"time"
+
+	"github.com/minio/minio-go/v7"
+	"github.com/raziel2244/geckosite/database"
+	"github.com/raziel2244/geckosite/database/model"
+	"github.com/raziel2244/geckosite/s3"
 )
 
 // Home returns the home page.
 func Home(w http.ResponseWriter, r *http.Request) {
-	type card struct{ Title, Image, Link string }
+	type card struct{ Title, Path, Image string }
 	type page struct {
 		Cards []card
 	}
 
 	data := page{
 		Cards: []card{
-			{"Crested Geckos", "/static/img/hhicon.png", "/geckos/crested"},
-			{"Gargoyle Geckos", "/static/img/hhicon.png", "/geckos/gargoyle"},
-			{"Leopard Geckos", "/static/img/hhicon.png", "/geckos/leopard"},
+			{"Crested Geckos", "/geckos/crested", ""},
+			{"Gargoyle Geckos", "/geckos/gargoyle", ""},
+			{"Leopard Geckos", "/geckos/leopard", ""},
 		},
+	}
+
+	rand.Seed(time.Now().Unix())
+
+	for c, card := range data.Cards {
+		splitPath := strings.Split(card.Path, "/")
+
+		var species model.Species
+		database.DB.
+			Where(&model.Species{Order: splitPath[1], Type: splitPath[2]}).
+			First(&species)
+
+		var animals []*model.Animal
+		// check for sale
+		// check holdbacks
+		// check personal
+
+		wheres := []string{
+			"status = 'For Sale'",
+			"status = 'Holdback'",
+			"status IN ('Non-Breeder','Breeder','Future Breeder')",
+		}
+
+		for _, where := range wheres {
+			database.DB.Where("species_id = ? AND "+where, species.ID.String()).Find(&animals)
+			if len(animals) > 0 {
+				break
+			}
+		}
+
+		if len(animals) == 0 {
+			data.Cards[c].Image = "/static/img/coming-soon.jpg"
+			continue
+		}
+
+		for _, animal := range animals {
+			// get images for animal
+			var images []string
+			ch := s3.Client.ListObjects(
+				context.Background(),
+				splitPath[1],
+				minio.ListObjectsOptions{
+					Prefix:    splitPath[2] + "/" + animal.ID.String(),
+					Recursive: true,
+				},
+			)
+			for object := range ch {
+				path := "/s3/" + splitPath[1] + "/" + object.Key
+				images = append(images, path)
+			}
+
+			// pick random one for card
+			if len(images) > 0 {
+				data.Cards[c].Image = images[rand.Intn(len(images))]
+				break
+			}
+		}
 	}
 
 	lp, hp := "templates/layout.gohtml", "templates/home.gohtml"
