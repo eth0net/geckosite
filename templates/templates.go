c2f8e05fbaf7962bf2acb094ec445affcefb8236
Elliot Thomas <elliot@eth0net.io>
2021-04-28 15:37:46 +0100

    Fixed missing copyright function on animal page

--- templates/templates.go
+++ templates/templates.go
@@ -11,27 +11,28 @@ import (
 //go:embed *.gohtml
 var fs embed.FS
 
+var baseTemplateFuncMap = template.FuncMap{
+	"copyright": func(holder string, start int) string {
+		var year string
+		now := time.Now().Year()
+		if start > 0 && start < now {
+			year += strconv.Itoa(start) + "-"
+		}
+		year += strconv.Itoa(now)
+
+		return fmt.Sprintf("Ⓒ Copyright %s %s", year, holder)
+	},
+}
+
 // Parse takes a template name and returns the parsed template from the embedded FS.
 func Parse(name string) *template.Template {
-	f := template.FuncMap{
-		"copyright": func(holder string, start int) string {
-			var year string
-			now := time.Now().Year()
-			if start > 0 && start < now {
-				year += strconv.Itoa(start) + "-"
-			}
-			year += strconv.Itoa(now)
-
-			return fmt.Sprintf("Ⓒ Copyright %s %s", year, holder)
-		},
-	}
-
-	t := template.New(name).Funcs(f)
+	t := template.New(name)
 	return ParseInto(t, name)
 }
 
 // ParseInto takes an existing Template pointer and parses
 // the specified template from the embedded FS into it.
 func ParseInto(t *template.Template, name string) *template.Template {
+	t = t.Funcs(baseTemplateFuncMap)
 	return template.Must(t.ParseFS(fs, "layout.gohtml", name+".gohtml"))
 }
